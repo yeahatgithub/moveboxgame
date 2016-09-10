375491ab666201cd77b400efd74c9816066fd252
Changchun Ye <chchye@nudt.edu.cn>
2016-09-10 17:38:06 +0800

    实现了： 1. 在游戏局面中搬运工走动，推箱子移动的功能。只考虑了一个箱子的情形。 2. 箱子到达红旗位置时，报告游戏过关。只考虑了一个箱子的情形。

--- app/src/main/java/com/yescorp/moveboxgame/GameState.java
+++ app/src/main/java/com/yescorp/moveboxgame/GameState.java
@@ -6,17 +6,199 @@ package com.yescorp.moveboxgame;
 public class GameState {
     private int mManRow;
     private int mManColumn;
+    private int mBoxRow;
+    private int mBoxColumn;
+    private int mFlagRow;  //红旗行号
+    private int mFlagColumn;  //红旗列号
+
+    public int getManRow() {
+        return mManRow;
+    }
+
+    public int getManColumn() {
+        return mManColumn;
+    }
+
+    public int getBoxRow() {
+        return mBoxRow;
+    }
+
+    public int getBoxColumn() {
+        return mBoxColumn;
+    }
+
+    public void setBoxColumn(int boxColumn) {
+        mBoxColumn = boxColumn;
+    }
+
+    public void setBoxRow(int boxRow) {
+        mBoxRow = boxRow;
+    }
+
+    public void setManRow(int manRow) {
+        mManRow = manRow;
+    }
+
+    public void setManColumn(int manColumn) {
+        mManColumn = manColumn;
+    }
 
     private StringBuffer[] mLabelInCells;
 
+    //构造函数
+    //什么时候执行？
+    //   在生成GameState类实例的时候
+    //参数：
+    //   initialState  某一关的开局
     public GameState(String[] initialState){
+        //为二维矩阵mLabelInCells分配存储空间。每个元素是StringBuffer的引用
         mLabelInCells = new StringBuffer[initialState.length];
+        //为每一个元素分配StringBuffer对象
         for (int i = 0; i < initialState.length; i++)
+            //生成第i+1个元素的StringBuffer对象
             mLabelInCells[i] = new StringBuffer(initialState[i]);
+        get_gongren_chushi_weizhi();   //根据游戏开局初始化搬运工的位置
+        get_XiangZi_ChuShi_WeiZhi();   //根据开局初始化箱子的位置。假设游戏界面中只有一个箱子。
+        get_HongQi_ChuShi_WeiZhi();
+
     }
 
+    public boolean isGameOver(){
+        return mBoxRow == mFlagRow && mBoxColumn == mFlagColumn;
+    }
+
+
+    private void get_HongQi_ChuShi_WeiZhi() {
+        for (int r = 0; r < GameView.CELL_NUM_PER_LINE; r++)
+            for (int c = 0; c < GameView.CELL_NUM_PER_LINE; c++){
+                if (mLabelInCells[r].charAt(c) == 'F')  {
+                    mFlagRow = r;
+                    mFlagColumn = c;
+                    return;
+                }
+            }
+    }
+
+    //获得表示局面的二维矩阵
     public StringBuffer[] getLabelInCells() {
         return mLabelInCells;
+    }
+    //根据游戏开局初始化搬运工的位置
+    public void get_gongren_chushi_weizhi() {
+        //StringBuffer[] labelInCells = getLabelInCells();
+        for (int r = 0; r < GameView.CELL_NUM_PER_LINE; r++)
+            for (int c = 0; c < GameView.CELL_NUM_PER_LINE; c++){
+                if (mLabelInCells[r].charAt(c) == 'M')  {
+                    mManRow = r;
+                    mManColumn = c;
+                    return;
+                }
+            }
+    }
+
+    //根据开局初始化箱子的位置
+    public void get_XiangZi_ChuShi_WeiZhi(){
+        for (int r=0; r<GameView.CELL_NUM_PER_LINE; r++)
+            for (int c = 0; c < GameView.CELL_NUM_PER_LINE; c++){
+                if (mLabelInCells[r].charAt(c) == 'B'){
+                    mBoxRow = r;
+                    mBoxColumn = c;
+                    return;
+                }
+            }
+
+    }
+
+    public void handleLeft() {
+        if (isBoxLeftToMan()){
+            if (mBoxColumn > 0  && mLabelInCells[mBoxRow].charAt(mBoxColumn - 1) != 'W'){
+                mLabelInCells[mBoxRow].setCharAt(mBoxColumn, ' ');
+                mLabelInCells[mBoxRow].setCharAt(mBoxColumn - 1, 'B');
+                mBoxColumn--;
+                mLabelInCells[mManRow].setCharAt(mManColumn, ' ');
+                mLabelInCells[mManRow].setCharAt(mManColumn - 1, 'M');
+                mManColumn--;
+            }
+        }else if (mManColumn > 0 && mLabelInCells[mManRow].charAt(mManColumn - 1) != 'W') {
+            mLabelInCells[mManRow].setCharAt(mManColumn, ' ');
+            mLabelInCells[mManRow].setCharAt(mManColumn - 1, 'M');
+            mManColumn--;
+        }
+    }
+
+    public boolean isBoxLeftToMan() {
+        return mBoxRow == mManRow && mBoxColumn == mManColumn - 1;
+    }
+
+    public void handleAbove() {
+        //StringBuffer[] labelInCells = mGameActivity.getCurrentState().getLabelInCells();
+        if (isBoxAboveMan()){
+            if (mBoxRow > 0 && mLabelInCells[mBoxRow - 1].charAt(mBoxColumn) != 'W'){  //箱子没出界 并且 到达单元格不是墙
+                //移动箱子，修改受影响的单元格所对应的矩阵元素的值
+                mLabelInCells[mBoxRow].setCharAt(mBoxColumn, ' ');
+                mLabelInCells[mBoxRow - 1].setCharAt(mBoxColumn, 'B');
+                mBoxRow--;  //修改记录箱子的位置的变量的值
+
+                //工人走动，修改受影响的单元格所对应的矩阵元素的值
+                mLabelInCells[mManRow].setCharAt(mManColumn, ' ');
+                mLabelInCells[mManRow - 1].setCharAt(mManColumn, 'M');
+                mManRow--; //修改记录工人的位置的变量的值
+            }
+        } else if (mManRow > 0 && mLabelInCells[mManRow - 1].charAt(mManColumn) != 'W') { //搬运工没出界，并且 到达单元格不是墙
+            //工人走动，修改受影响的单元格所对应的矩阵元素的值
+            mLabelInCells[mManRow].setCharAt(mManColumn, ' ');
+            mLabelInCells[mManRow - 1].setCharAt(mManColumn, 'M');
+            mManRow--; //修改记录工人的位置的变量的值
+        }
+    }
+
+    public boolean isBoxAboveMan() {
+        return mBoxColumn == mManColumn && mBoxRow == mManRow - 1;
+    }
+
+    public void handleDown() {
+        if (isBoxBlowMan()) {
+            if (mBoxRow + 1 < GameView.CELL_NUM_PER_LINE) {
+                //移动箱子，修改受影响的单元格所对应的矩阵元素的值
+                mLabelInCells[mBoxRow].setCharAt(mBoxColumn, ' ');
+                mLabelInCells[mBoxRow + 1].setCharAt(mBoxColumn, 'B');
+                mBoxRow++;
+                //工人走动，修改受影响的单元格所对应的矩阵元素的值
+                mLabelInCells[mManRow].setCharAt(mManColumn, ' ');
+                mLabelInCells[mManRow + 1].setCharAt(mManColumn, 'M');
+                mManRow++;
+            }
+        } else if (mManRow + 1 < GameView.CELL_NUM_PER_LINE) {
+            mLabelInCells[mManRow].setCharAt(mManColumn, ' ');
+            mLabelInCells[mManRow + 1].setCharAt(mManColumn, 'M');
+            mManRow++;
+        }
+    }
+
+    public void handleRight() {
+        //StringBuffer[] labelInCells = mGameActivity.getCurrentState().getLabelInCells();
+        if (isBoxRightToMan()) {
+            if (mBoxColumn + 1 < GameView.CELL_NUM_PER_LINE) {
+                mLabelInCells[mBoxRow].setCharAt(mBoxColumn, ' ');
+                mLabelInCells[mBoxRow].setCharAt(mBoxColumn + 1, 'B');
+                mBoxColumn++;
+                mLabelInCells[mManRow].setCharAt(mManColumn, ' ');
+                mLabelInCells[mManRow].setCharAt(mManColumn + 1, 'M');
+                mManColumn++;
+            }
+        } else if (mManColumn + 1 < GameView.CELL_NUM_PER_LINE) {
+            mLabelInCells[mManRow].setCharAt(mManColumn, ' ');
+            mLabelInCells[mManRow].setCharAt(mManColumn + 1, 'M');
+            mManColumn++;
+        }
+    }
+
+    public boolean isBoxRightToMan() {
+        return mBoxRow == mManRow && mBoxColumn == mManColumn + 1;
+    }
+
+    public boolean isBoxBlowMan() {
+        return mBoxColumn == mManColumn && mBoxRow == mManRow + 1;
     }
 
 
--- app/src/main/java/com/yescorp/moveboxgame/GameView.java
+++ app/src/main/java/com/yescorp/moveboxgame/GameView.java
@@ -16,17 +16,17 @@ public class GameView extends View{
     private float mCellWidth;
     public static final int CELL_NUM_PER_LINE = 12;
 
-    private int mManRow = 0;
-    private int mManColumn = 0;
-    private int mBoxRow = 5;      //箱子一开始处在游戏区域中间位置
-    private int mBoxColumn = 5;
+//    private int mManRow = 0;      //搬运工当前位置的行号
+//    private int mManColumn = 0;   //搬运工当前位置的列号
+//    private int mBoxRow = 5;      //箱子一开始处在游戏区域中间位置
+//    private int mBoxColumn = 5;
 
     private GameActivity mGameActivity;
 
     public GameView(Context context) {
         super(context);
         mGameActivity = (GameActivity) context;
-        GameBitmaps.loadGameBitmaps(getResources());
+       GameBitmaps.loadGameBitmaps(getResources());  //加载图片
     }
 
     //当GameView实例的尺寸发生变化，就会调用onSizeChanged
@@ -65,18 +65,32 @@ public class GameView extends View{
 
         //根据游戏局面绘制游戏界面
         drawGameBoard(canvas);
+
+        //如果过关了，要报告过关
+        if(mGameActivity.getCurrentState().isGameOver()) {
+            Paint txtPaint = new Paint();
+            txtPaint.setColor(Color.RED);
+            txtPaint.setTextSize(100.0f);
+            //报告过关了
+            canvas.drawText(getContext().getString(R.string.txt_game_over), 4 * mCellWidth, 6 * mCellWidth, txtPaint );
+        }
+
     }
 
     private void drawGameBoard(Canvas canvas) {
-        Rect srcRect;
-        Rect destRect;
+        Rect srcRect;       //表示图片内部的区域
+        Rect destRect;      //表示图片在屏幕中显示的区域
+        //得到表示局面的二维矩阵
         StringBuffer [] labelInCells = mGameActivity.getCurrentState().getLabelInCells();
-        for (int r = 0; r < labelInCells.length; r++)
-            for (int c = 0; c < labelInCells[r].length(); c++){
-                destRect = getRect(r, c);
+
+        for (int r = 0; r < labelInCells.length; r++)  //逐行地扫描矩阵
+            for (int c = 0; c < labelInCells[r].length(); c++){ //对当前行r，逐列地扫描
+                destRect = getRect(r, c);  //得到图片在屏幕中的显示区域
                 switch (labelInCells[r].charAt(c)){
                     case 'W':
+                        //得到墙体图片的完整区域
                         srcRect = new Rect(0, 0, GameBitmaps.WallBitmap.getWidth(), GameBitmaps.WallBitmap.getHeight());
+                        //绘制墙体图片
                         canvas.drawBitmap(GameBitmaps.WallBitmap, srcRect, destRect, null);
                         break;
                     case 'B':
@@ -97,78 +111,29 @@ public class GameView extends View{
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        //忽略UP, MOVE型的动作的处理
         if (event.getAction() != MotionEvent.ACTION_DOWN)
             return true;
 
         int touch_x = (int) event.getX();   //触摸点的x坐标
         int touch_y = (int) event.getY();   //触摸点的y坐标
-        if (touch_blow_to_man(touch_x, touch_y, mManRow, mManColumn))  //按在下方
-            handleDown();
+        if (touch_blow_to_man(touch_x, touch_y,
+                mGameActivity.getCurrentState().getManRow(),
+                mGameActivity.getCurrentState().getManColumn()))  //按在下方
+            mGameActivity.getCurrentState().handleDown();
 
-        if (touch_right_to_man(touch_x, touch_y, mManRow, mManColumn))  //按在右侧
-            handleRight();
-        if (touch_above_to_man(touch_x, touch_y, mManRow, mManColumn))
-            handleAbove();
-        if (touch_left_to_man(touch_x, touch_y, mManRow, mManColumn))
-            handleLeft();
+        if (touch_right_to_man(touch_x, touch_y,
+                mGameActivity.getCurrentState().getManRow(),
+                mGameActivity.getCurrentState().getManColumn()))  //按在右侧
+            mGameActivity.getCurrentState().handleRight();
+        if (touch_above_to_man(touch_x, touch_y, mGameActivity.getCurrentState().getManRow(),
+                mGameActivity.getCurrentState().getManColumn()))
+            mGameActivity.getCurrentState().handleAbove();
+        if (touch_left_to_man(touch_x, touch_y, mGameActivity.getCurrentState().getManRow(),
+                mGameActivity.getCurrentState().getManColumn()))
+            mGameActivity.getCurrentState().handleLeft();
         postInvalidate();
         return true;
-    }
-
-    private void handleLeft() {
-        if (isBoxLeftToMan()){
-            if (mBoxColumn > 0){
-                mBoxColumn--;
-                mManColumn--;
-            }
-        }else if (mManColumn > 0)
-            mManColumn--;
-    }
-
-    private boolean isBoxLeftToMan() {
-        return mBoxRow == mManRow && mBoxColumn == mManColumn - 1;
-    }
-
-    private void handleAbove() {
-        if (isBoxAboveMan()){
-            if (mBoxRow > 0){
-                mBoxRow--;
-                mManRow--;
-            }
-        } else if (mManRow > 0)
-            mManRow--;
-    }
-
-    private boolean isBoxAboveMan() {
-        return mBoxColumn == mManColumn && mBoxRow == mManRow - 1;
-    }
-
-    private void handleDown() {
-        if (isBoxBlowMan()) {
-            if (mBoxRow + 1 < CELL_NUM_PER_LINE) {
-                mBoxRow++;
-                mManRow++;
-            }
-        } else if (mManRow + 1 < CELL_NUM_PER_LINE)
-            mManRow++;
-    }
-
-    private void handleRight() {
-        if (isBoxRightToMan()) {
-            if (mBoxColumn + 1 < CELL_NUM_PER_LINE) {
-                mBoxColumn++;
-                mManColumn++;
-            }
-        } else if (mManColumn + 1 < CELL_NUM_PER_LINE)
-            mManColumn++;
-    }
-
-    private boolean isBoxRightToMan() {
-        return mBoxRow == mManRow && mBoxColumn == mManColumn + 1;
-    }
-
-    private boolean isBoxBlowMan() {
-        return mBoxColumn == mManColumn && mBoxRow == mManRow + 1;
     }
 
 
